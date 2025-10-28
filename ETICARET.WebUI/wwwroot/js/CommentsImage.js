3c6c7a335f577c83bbad3fa98915456d2f4cc0b7
yagizonur <yagiz.onur@outlook.com>
2025-10-28 20:42:32 +0300

    ...

---
+++ ETICARET.WebUI/wwwroot/js/CommentsImage.js
@@ -0,0 +1,105 @@
+﻿CommentBodyId = "#comment";
+var productId = -1;
+
+function imageBox(smallImg) {
+    var fullImg = document.getElementById("image-box");
+    fullImg.src = smallImg.src
+}
+
+$(document).ready(function () {
+    var url = $("#comment").data("url");
+    $("#comment").load(url)
+    productId = $("#comment").data("product-id")
+    $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+})
+
+function doComment(btn, e, commentId, spanId) {
+    var button = $(btn);
+    var mode = button.data("edit-mode");
+    var editableContent = $("#comment_text_" + commentId)
+
+    if (e == "new_clicked") {
+        var txt = $("#new_comment_text").val();
+
+        $.ajax({
+            method: "POST",
+            url: '/Comment/Create',
+            data: { 'text': txt, 'productId': productId }
+
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+            }
+            else {
+                alert("Yorum yapılamadı!")
+            }
+        })
+            .fail(function (e) {
+                alert("Sunucuda tarafında bir hata oluştu!")
+            })
+    }
+
+    else if (e == "delete_clicked") {
+        var dialog_res = confirm("Yorum silinsin mi?")
+
+        if (!dialog_res) return false;
+
+        $.ajax({
+            method: "POST",
+            url: "/Comment/Delete?id=" + commentId,
+        }).done(function (data) {
+            if (data.result) {
+                $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+            }
+            else {
+                alert("Yorum silinemedi!")
+            }
+        }).fail(function (e) {
+            alert("Sunucu tarafında bir hata oluştu!")
+        })
+    }
+
+    else if (e == "edit_clicked") {
+        if (!mode) {
+            button.data("edit-mode", true)
+            button.removeClass("btn-warning")
+            button.addClass("btn-success")
+            var btnSpan = button.find("span")
+            btnSpan.removeClass("fa-edit")
+            btnSpan.addClass("fa-check")
+            editableContent.addClass("editable-content")
+            editableContent.addClass("editableComment")
+
+            $(spanId).attr("contenteditable", true)
+        }
+        else {
+            button.data("edit-mode", true)
+            button.removeClass("btn-sucess")
+            button.addClass("btn-warning")
+            var btnSpan = button.find("span")
+            btnSpan.removeClass("fa-check")
+            btnSpan.addClass("fa-edit")
+            editableContent.removeClass("editable-content")
+            editableContent.removeClass("editableComment")
+
+            $(spanId).attr("contenteditable", true)
+
+            var txt = $(spanId).text();
+
+            $.ajax({
+                method: "POST",
+                url: "/Comment/Edit",
+                data: {'text':txt,"id":commentId}
+            }).done(function (data) {
+                if (data.result) {
+                    $(CommentBodyId).load("/Comment/ShowProductComments?id=" + productId)
+                }
+                else {
+                    alert("Yorum güncellenemedi!")
+                }
+            }).fail(function (e) {
+                alert("Sunucu tarafında bir hata oluştu!")
+            })
+        }
+    }
+}
